e620eb1c9c031c78e83cc6a52af21a3be92a26f9
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-14 17:40:00 -0300

    initial commit

---
+++ src/Auctions.API/Controllers/AuctionController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Auctions.API.UseCases.Auctions.GetCurrent;
+
+namespace Auctions.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult CurrentAuction()
+    {
+        var useCase = new GetCurrentAuctionUseCase();
+        var result = useCase.Execute();
+
+        return Ok(result);
+    }
+}
