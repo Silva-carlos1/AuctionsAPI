5fc93439ff44a2eec28d053c2868767f44bb5b31
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-16 15:55:54 -0300

    feat: improve auction retrieval logic and Swagger documentation

--- src/Auctions.API/Controllers/AuctionController.cs
+++ src/Auctions.API/Controllers/AuctionController.cs
@@ -8,10 +8,16 @@ namespace Auctions.API.Controllers;
 public class AuctionController : ControllerBase
 {
     [HttpGet]
+    [ProducesResponseType(typeof(Auction), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult CurrentAuction()
     {
         var useCase = new GetCurrentAuctionUseCase();
+
         var result = useCase.Execute();
+
+        if (result is null)
+            return NoContent();
 
         return Ok(result);
     }
--- src/Auctions.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/Auctions.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -6,13 +6,16 @@ namespace Auctions.API.UseCases.Auctions.GetCurrent;
 
 public class GetCurrentAuctionUseCase
 {
-    public Auction Execute()
+    public Auction? Execute()
     {
         var repository = new AuctionsDbContext();
+
+        var today = DateTime.Now;
+
 
         return repository
             .Auctions
             .Include(auction =>  auction.Items)
-            .First();
+            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
     }
 }
