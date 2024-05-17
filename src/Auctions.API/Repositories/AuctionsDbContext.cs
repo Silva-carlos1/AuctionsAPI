b404cc8899ba38f6e53f65e739c2d29b7dae6f79
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-17 13:04:52 -0300

    feat: query Offers on DB

--- src/Auctions.API/Repositories/AuctionsDbContext.cs
+++ src/Auctions.API/Repositories/AuctionsDbContext.cs
@@ -7,6 +7,7 @@ public class AuctionsDbContext : DbContext
 {
     public DbSet<Auction> Auctions { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Offer> Offers { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=C:\\Users\\duduo\\OneDrive\\Documentos\\C#\\auction.db");
