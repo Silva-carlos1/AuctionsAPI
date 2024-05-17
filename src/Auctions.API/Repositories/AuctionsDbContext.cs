61ccd20d91f0443958a8869366f986129164278d
Carlos <silva.carloseduardo.ds@gmail.com>
2024-05-17 11:02:34 -0300

    feat: query Users on DB

--- src/Auctions.API/Repositories/AuctionsDbContext.cs
+++ src/Auctions.API/Repositories/AuctionsDbContext.cs
@@ -6,6 +6,7 @@ namespace Auctions.API.Repositories;
 public class AuctionsDbContext : DbContext
 {
     public DbSet<Auction> Auctions { get; set; }
+    public DbSet<User> Users { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=C:\\Users\\duduo\\OneDrive\\Documentos\\C#\\leilaoDbNLW.db");
